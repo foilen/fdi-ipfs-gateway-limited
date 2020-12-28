1b461cf47ade999ca78c3b8f2d31f8af4d6fb4a5
Simon Levesque <simon@foilen.com>
2020-12-28 15:12:29 -0500

    Upgrade Gateway Limited to 1.0.1

--- step-create-docker-image.sh
+++ step-create-docker-image.sh
@@ -5,19 +5,22 @@ set -e
 RUN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $RUN_PATH
 
+IPFS_GATEWAY_LIMITED_VERSION=1.0.1
+IPFS_VERSION=0.7.0
+
 echo ----[ Download assets ]----
 mkdir -p _assets
 cd _assets
 
-if [ ! -f ipfs-gateway-limited_1.0.0_amd64.deb ] ; then
-    wget https://dl.bintray.com/foilen/debian/ipfs-gateway-limited_1.0.0_amd64.deb
-    ar x ipfs-gateway-limited_1.0.0_amd64.deb
+if [ ! -f ipfs-gateway-limited_${IPFS_GATEWAY_LIMITED_VERSION}_amd64.deb ] ; then
+    wget https://dl.bintray.com/foilen/debian/ipfs-gateway-limited_${IPFS_GATEWAY_LIMITED_VERSION}_amd64.deb
+    ar x ipfs-gateway-limited_${IPFS_GATEWAY_LIMITED_VERSION}_amd64.deb
     tar -Jxf data.tar.xz
 fi
 
-if [ ! -f go-ipfs_v0.7.0_linux-amd64.tar.gz ] ; then
-    wget https://dist.ipfs.io/go-ipfs/v0.7.0/go-ipfs_v0.7.0_linux-amd64.tar.gz
-    tar -zxf go-ipfs_v0.7.0_linux-amd64.tar.gz
+if [ ! -f go-ipfs_v${IPFS_VERSION}_linux-amd64.tar.gz ] ; then
+    wget https://dist.ipfs.io/go-ipfs/v${IPFS_VERSION}/go-ipfs_v${IPFS_VERSION}_linux-amd64.tar.gz
+    tar -zxf go-ipfs_v${IPFS_VERSION}_linux-amd64.tar.gz
 fi
 
 echo ----[ Build docker image ]----
